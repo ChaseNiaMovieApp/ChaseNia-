cbe30b212b9d458e98a1cc6dcfa125e8f5067392
Chase Martinez <chasemartinez22@gmail.com>
2022-06-01 11:39:34 -0500

    Feat page removes redundant, empty or non-essential objects

--- js/movie-app.js
+++ js/movie-app.js
@@ -8,20 +8,23 @@ const baseURL = "https://exclusive-radical-peak.glitch.me/movies/";
 fetch(baseURL)
     .then(res => res.json())
     .then(res => { // array of movies
-        //
+        console.log(res)
 
         for (let i = 0; i < res.length; i++) {
+            console.log(res[i]);
             // call the delete method
             // do it when the title is missing fron the record.
-
+            // console.log(res[i]);
             let settings = {
                 method: "DELETE"
             }
-            if (res[i].title === undefined) {
+            if (res[i].title === "" || res[i].title === "1" || res[i].title === 1 || res[i].title === "hello" || res[i].title === undefined) {
+                fetch(baseURL + res[i].id, settings)
+            }
+            if (res[i].id === 283 || res[i].id === 284) {
                 fetch(baseURL + res[i].id, settings)
             }
         }
-        console.log(res)
     });
 
 // TODO delete posts
