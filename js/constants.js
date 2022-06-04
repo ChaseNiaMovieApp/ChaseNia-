becc4ff5921d993a4bc65cc683edec6d9395aa14
Chase Martinez <chasemartinez22@gmail.com>
2022-06-04 18:46:00 -0500

    refactor contants.js to use normal string quotes instead of template strings

--- js/constants.js
+++ js/constants.js
@@ -1,5 +1,5 @@
-export const imgURL = `http://img.omdbapi.com/?i=`;
-export const titleURL = `http://www.omdbapi.com/?t=`;
+export const imgURL = "http://img.omdbapi.com/?i=";
+export const titleURL = "http://www.omdbapi.com/?t=";
 
 export const fetchSettings = {
     method: "GET",
